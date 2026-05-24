4c72f325bbe55341c3cb4c69eb9a682e6134c78d
caiolima-rgb <caioerosx@gmail.com>
2026-05-23 21:28:45 -0300

    código base do prof

---
+++ aulas/atividade avaliativa/código.c
@@ -0,0 +1,26 @@
+//código do comsumo energetico
+#include <stdio.h>
+int main(void){
+    float consumo[3][5];
+    float somasetor,totalgeral=0;
+
+    int setormaior=0,diamaior=0;
+    int i, j;
+
+    //aqui é onde o usuário irá inserir os dados de consumo energético
+    for(i=0;i<3;i++){
+        printf("Digite o consumo do setor %d:\n", i+1);
+        for(j=0;j<5;j++){
+            printf("Digite o consumo do dia %d: ", j+1);
+            scanf("%f", &consumo[i][j]);
+        }
+    }
+    //painel de resultados
+    for(i=0;i<3,i++){
+        for(j=0,j<5;j++){
+            printf("%1.f\t", consumo[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
