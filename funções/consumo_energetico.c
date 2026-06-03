64a3ebd1e579b599814407fe7aa16a2e0d350f7b
ersx777 <caioerosx@gmail.com>
2026-06-03 08:29:59 -0300

    ADICIONADO O MENU

---
+++ funções/consumo_energetico.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+
+Criar um programa em C que realize as seguintes tarefas:
+
+1. **Entrada de Dados:** Solicitar ao usuário o nome dos eletrodomésticos que possui e o consumo em kWh da etiqueta (ex: Ventilador 0.12 kWh). Limite de **somente 3** eletrodomésticos.
+2. **Processamento (Consumo Mensal):** Mostrar o consumo total do mês, considerando que todos os eletrodomésticos informados ficam funcionando todos os dias, sem desligar, durante o mês inteiro (24h por dia, 30 dias).
+3. **Saída (Fatura Financeira):** Mostrar o valor total da fatura, considerando que o custo de 1 kWh é de **R$ 1,25**.
+
+*/
+
+int main(void){
+    int i;
+    float consumo[3];
+    float consumototal;
+    char tipodeeletro[3][20];
+    int opcao = 0;
+
+    while (opcao != 4){
+        printf("===============CONSUMO ENERGETICO===============\n");
+        printf("Escolha um opcao: \n");
+        printf("1.Inserir consumo\n");
+        printf("2.Consumo mensal\n");
+        printf("3. Emitir fatura\n");
+        printf("4.Sair \n");
+        scanf("%d", &opcao);
+        if (opcao == 0 || opcao < 0 || opcao > 4){
+            printf("DIgite um opcao valida!\n");
+        }
+    }
+}
